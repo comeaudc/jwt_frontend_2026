d644d5c4e2ad064bcd80aaca6adc961b4d345d02
DylanC <comeaudc@comcast.net>
2026-02-19 17:37:12 -0500

    Added logout

--- src/pages/dashboard/Index.jsx
+++ src/pages/dashboard/Index.jsx
@@ -1,7 +1,19 @@
+import { useAuth } from "../../context/authContext/AuthContext.jsx";
+import { useNavigate } from "react-router-dom";
+
 const Dashboard = () => {
+  const { logout } = useAuth();
+  const nav = useNavigate();
+
+  function handleLogout() {
+    logout();
+    nav("/auth");
+  }
+
   return (
     <div>
       <h1>Only Users Should See This Page</h1>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 };
